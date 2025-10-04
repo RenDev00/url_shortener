a3b101112b6f843f760d53f2a34b8b710749c26e
RenDev00 <rene.huber00@gmx.at>
2025-10-04 10:50:43 +0200

    Implemented routes for shortening urls and redirecting to original urls

---
+++ src/main.py
@@ -0,0 +1,40 @@
+import random
+import string
+from flask import Flask, render_template, redirect, request
+
+import db_connector
+
+app = Flask(__name__)
+
+
+def get_short_code(original_url: str) -> str:
+    short_code = "".join(
+        random.choices(
+            string.ascii_lowercase + string.ascii_uppercase + string.digits, k=6
+        )
+    )
+    db_connector.add_url(original_url, short_code)
+    return short_code
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        original_url = request.form["original-url"]
+        shortened_url = f"{request.host_url}{get_short_code(original_url)}"
+        return render_template("index.html", shortened_url=shortened_url)
+    else:
+        return render_template("index.html")
+
+
+@app.route("/<short_code>")
+def shortened_url(short_code: str = None):
+    original_url = db_connector.get_url(short_code)
+    if original_url:
+        return redirect(original_url)
+
+    return "Page not found", 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
