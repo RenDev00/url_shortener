547f6a41db9fbb8c39eb3f3916ee1cba16fb5992
RenDev00 <rene.huber00@gmx.at>
2025-10-10 13:18:17 +0200

    Updated tests to accomodate for deletion of expired urls

--- tests/test_db_connector.py
+++ tests/test_db_connector.py
@@ -13,15 +13,20 @@ def sample_short_code() -> str:
     return "abcdef"
 
 
-def test_add_and_get_url(sample_url, sample_short_code):
-    db_connector.add_url(sample_url, sample_short_code)
+@pytest.fixture
+def sample_validity_days() -> int:
+    return -1
+
+
+def test_add_and_get_url(sample_url, sample_short_code, sample_validity_days):
+    db_connector.add_url(sample_url, sample_short_code, sample_validity_days)
 
     assert db_connector.get_url(sample_short_code) == sample_url
 
 
-def test_add_existing_url(sample_url, sample_short_code):
+def test_add_existing_url(sample_url, sample_short_code, sample_validity_days):
     with pytest.raises(supabase.PostgrestAPIError):
-        db_connector.add_url(sample_url, sample_short_code)
+        db_connector.add_url(sample_url, sample_short_code, sample_validity_days)
 
 
 def test_increment_clicks(sample_short_code):
@@ -35,3 +40,11 @@ def test_delete_short_code(sample_short_code):
 
     assert db_connector.get_data(sample_short_code) == None
     assert db_connector.get_url(sample_short_code) == None
+
+
+def test_delete_expired_urls(sample_url, sample_short_code, sample_validity_days):
+    db_connector.add_url(sample_url, sample_short_code, sample_validity_days)
+    db_connector.delete_expired_urls()
+
+    assert db_connector.get_data(sample_short_code) == None
+    assert db_connector.get_url(sample_validity_days) == None
